00ac273638aa145bcb1ecb6630eeb1c5b8cb67b5
Szymon Rutkowski <szymon@szymonrutkowski.pl>
2019-01-12 22:57:57 +0100

    working on dobrzyńskie

--- load.py
+++ load.py
@@ -1,12 +1,12 @@
-import json, os, re
+import csv, io, json, os, re
 from load_helpers import is_heading, is_meta_line
-from sys import argv
+from sys import argv, exit
 
-if len(argv) != 3:
+if len(argv) != 2:
     print('USAGE: python3 load.py CONFIG_FILE')
-    os.exit(-1)
+    exit(-1)
 
-config_file_path = argv[2]
+config_file_path = argv[1]
 
 # Load the config
 config = None
@@ -30,14 +30,34 @@ class Section():
         self.author = config['default_convent_author']
         self.text = section_contents
 
+    def row_string(self):
+        string_output = io.StringIO()
+        csv_writer = csv.writer(string_output, quoting=csv.QUOTE_NONNUMERIC)
+        csv_writer.writerow([
+            self.book_title,
+            self.inbook_section_id,
+            self.inbook_document_id,
+            self.scan_pages,
+            self.book_pages,
+            self.section_title,
+            self.section_type,
+            self.date,
+            self.palatinate,
+            self.convent_location,
+            self.created_location,
+            self.author,
+            self.text
+            ])
+        return string_output.getvalue().strip()
+
 # Load all the files.
 pages = [] # as lists of lines
 for dirname, dirnames, filenames in os.walk(config['path']):
     filenames = sorted(filenames)
     for filename in filenames:
         if re.match('^'+config['prefix'], filename):
-            file_number = int(filename[len(config['prefix']):])
-            with open(filename) as text_file:
+            file_number = int(re.search('\\d+', filename).group(0))
+            with open(dirname + filename) as text_file:
                 page_lines = text_file.read().split('\n')
                 pages.append(page_lines)
 
@@ -53,26 +73,36 @@ current_document_id = 0 # section id is just len(sections)
 previous_was_heading = False # indicates whether the last line was a heading
 for page_lines in pages:
     for line in page_lines:
+        line = line.strip()
         if current_paragraph == '':
             current_paragraph = line
         else:
             current_paragraph += ' ' + line
 
-        a_heading = is_heading(current_paragraph, config)
 
         # Meta sections detection (what is not dected as meta, will be document).
-        if not a_heading and (len(line) > config['max_nonmeta_line_len'] or is_meta_line(line, config)):
+        if (len(line) > config['max_nonmeta_line_len'] or is_meta_line(line, config)):
             current_section_type = 'meta'
+            a_heading = False
+        else:
+            a_heading = is_heading(current_paragraph, config)
+###        if a_heading:
+###            print(current_paragraph, ' is a heading')
+###        else:
+###            print(current_paragraph, ' not a heading')
 
         # End paragraph - do the relevant thing with it.
-        if line[-1] == '.' or len(line) < config['min_inparagraph_line_len']:
+        if len(line) == 0 or (line[-1] == '.' or len(line) < config['min_inparagraph_line_len']):
             # Add heading.
             if a_heading:
                 # We have a new heading/title - add the document section that just ended.
-                if not previous_was_heading and current_document_data['title'] != '': # not the start of the book
+                ###print(current_document_section[:29], previous_was_heading, current_document_data['title'])
+                if (not previous_was_heading) and current_document_data['title'] != '': # not the start of the book
                     current_document_id += 1
                     section = Section('document', current_document_data['title'],
                                        current_document_section, current_document_id)
+                    sections.append(section)
+                    current_document_data['title'] = ''
                     current_document_section = ''
                 # Set the title for next document section.
                 if previous_was_heading: # append to the previous part of the heading
@@ -84,7 +114,7 @@ for page_lines in pages:
                 previous_was_heading = False
 
             # Add paragraph to meta section content, and reset type to document.
-            if not a_heading and current_section_type == 'meta':
+            if (not a_heading) and current_section_type == 'meta':
                 if current_meta_section == '':
                     current_meta_section = current_paragraph
                 else:
@@ -92,7 +122,8 @@ for page_lines in pages:
                 # Be biased to reset section type to document.
                 current_section_type = 'document'
             # Add paragraph to document section content, and commit meta section if needed.
-            elif not a_heading and current_section_type == 'document':
+            elif (not a_heading) and current_section_type == 'document':
+###                print('commiting on line', line)
                 if current_document_section == '':
                     current_document_section = current_paragraph
                 else:
--- load_helpers.py
+++ load_helpers.py
@@ -7,42 +7,44 @@ meta_signs = [ # characteristic elements for a meta section
             # "Rękopis"
             re.compile('^.?.?Rp\\.'),
             # evident footnote
-            re.compile('^[\\WiIvVxX]{1,3}.{1,70}$'),
+            re.compile('^[\\WiIvVxX]{1,3} [^\\\\]{1,40}$'),
             # number range
             re.compile('[0-9]-[0-9]'),
             # anachronistic vocabulary
-            re.compile('wsp[oöó0][lł|]czesn|Vol\\.|Fasc\\.|(Hal\\. Rel\\.)|(Castr\\. Hal\\.)|(Hal\\. Laud\\.)|(Cop\\. Castr\\.)|( zob\\.)|( tek[sś])|( str\\.)')
+            re.compile('(wsp[oöó0][lł)|(]czesn)|(Vol\\.)|(Fasc\\.)|([fF]ol\.)|(Hal\\. Rel\\.)|(Castr\\. Hal\\.)|(Hal\\. Laud\\.)|(Cop\\. Castr\\.)|(Lauda Dobrinensia)|(Monit\\.? Comit\\.? Pol\\.?)|( zob\\.)|( tek[sś])|( str\\.)')
         ]
 def is_meta_line(line, config):
     for sign in meta_signs:
-        if sign.match(line):
+        if sign.search(line):
+            ###print(sign, line)
             return True
     # If more than the half of the line of non-alphabetic
-    if len(re.sub('\\w', '', line)) / len(line) >= 0.5:
+    if len(line) > 0 and len(re.sub('\\w', '', line)) / len(line) >= 0.5:
         return True
     # If large percentage of tokens is abbreviated
-    if line.count('. ') / line.count(' ') >= 0.33:
-        return True
+    ###if line.count(' ') > 0 and line.count('. ') / line.count(' ') >= 0.33:
+    ###    return True
     return False
 
 # Headings detection.
 heading_signs = [ # characteristic elements in a heading
          # square brackets used to number sections in editions
-         re.compile('\\[.*\\]')
+         re.compile('^\\[.*\\]'),
          # numbering - also days, years
-         re.compile('\\d+')
+         re.compile('\\d+'),
          # numbers put in words
-         re.compile('pierwsz|drug|wt[oó]r|dwa|trz[ae][cd]z?|czwart|czter|pi[ąę][tc]|sz[óe][śs][tć]|si[eó]de?m|[oó][sś]i?e?m|dziewi[ęą][tć]|dzie[sś]i?[ęą][tcć]|na[śs][tć]|st[oa]|setn?|tysi[ęą]c|prim|secund|terti|quart|quint|se[xg]|vice|esim|cent|mille|( [xXvViI]{1,3} )')
+         re.compile('(pierwsz)|(drug)|(wt[oó]r)|(dwa)|(trz[ae][cd]z?)|(czwart)|(czter)|(pi[ąę][tc])|(sz[óe][śs][tć])|(si[eó]de?m)|([oó][sś]i?e?m)|(dziewi[ęą][tć])|(dzie[sś]i?[ęą][tcć])|(na[śs][tć])|(st[oa])|(setn?)|(tysi[ęą]c)|(prim)|(secund)|(terti)|(quart)|(quint)|(se[xg])|(vice)|(esim)|(cent)|(mille)|( [xXvViI]{1,3} )'),
          # months
-         re.compile('stycze?[nń]|luty?|marz?e?c|kwie[tc]i?e?[nń]|maj|czerwi?e?c|lipi?e?c|sierpi?e?[nń]|wrze[sś]i?e?[nń]|październik|listopad|grudz?i?e?[nń]|[ji]anuar|februar|mart|april|mai|[ji]u[nl]i|august|e?o?m?br')
+         re.compile('(stycze?[nń])|(luty?)|(marz?e?c)|(kwie[tc]i?e?[nń])|(maj)|(czerwi?e?c)|(lipi?e?c)|(sierpi?e?[nń])|(wrze[sś]i?e?[nń])|(październik)|(listopad)|(grudz?i?e?[nń])|([ji]anuar)|(februar)|(mart)|(april)|(mai)|([ji]u[nl]i)|(august)|(e?o?m?br)'),
          # titles
-        re.compile('Uchwał[ay]|Uniwersał|Laudu?m?a?|Instrukcy?j?[ae]|Instructio|Konfederacy?j?|Odpowiedź|P?okazowan|Manifest|Protestac|Reskrypt'),
+        re.compile('(Uchwał[ay])|(Uniwersał)|(Laudu?m?a?)|(Instrukcy?j?[ae])|(Instructio)|(Konfederacy?j?)|(Odpowiedź)|(P?okazowan)|(Manifest)|(Protestac)|(Reskrypt)|(Uniwersał)')
         ]
 def is_heading(section, config):
-    if len(section) > config['max_heading_len']:
+    if len(section) < 15 or len(section) > config['max_heading_len']:
         return False
 
-    signs = [s.match(section) for s in heading_signs]
+    signs = [s.search(section) for s in heading_signs]
     signs_count = len([s for s in signs if s])
+###    print(section, signs_count, 'signs')
 
-    return signs_count => 2 and signs_count > (len(section) / 10)
+    return signs_count >= 2 and len(section) > 0 and signs_count > (len(section) / 30)
