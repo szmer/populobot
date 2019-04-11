43d3e7c2c254a748e9df20675ad2fece62b579bc
Szymon Rutkowski <szymon@szymonrutkowski.pl>
2019-04-12 00:47:54 +0200

    more (anti)patterns for better title detection

--- load_helpers.py
+++ load_helpers.py
@@ -20,16 +20,13 @@ def is_meta_line(line, config):
         if sign.search(line):
             ###print(sign, line)
             return True
-    # If more than the half of the line of non-alphabetic
-    if len(line) > 0 and len(re.sub('\\w', '', line)) / len(line) >= 0.5:
-        return True
     # If large percentage of tokens is abbreviated
     ###if line.count(' ') > 0 and line.count('. ') / line.count(' ') >= 0.33:
     ###    return True
     return False
 
 # Headings detection.
-heading_signs = [ # characteristic elements in a heading
+heading_signs = ([ # characteristic elements in a heading
          # square brackets used to number sections in editions
          re.compile('^\\[.*\\]'),
          # numbering - also days, years
@@ -37,16 +34,30 @@ heading_signs = [ # characteristic elements in a heading
          # numbers put in words
          re.compile('(pierwsz)|(drug)|(wt[oó]r)|(dwa)|(trz[ae][cd]z?)|(czwart)|(czter)|(pi[ąę][tc])|(sz[óe][śs][tć])|(si[eó]de?m)|([oó][sś]i?e?m)|(dziewi[ęą][tć])|(dzie[sś]i?[ęą][tcć])|(na[śs][tć])|(st[oa])|(setn?)|(tysi[ęą]c)|(prim)|(secund)|(terti)|(quart)|(quint)|(se[xg])|(vice)|(esim)|(cent)|(mille)|( [xXvViI]{1,3} )'),
          # months
-         MONTHS,
-         # titles
-        re.compile('(Uchwał[ay])|(Uniwersał)|(Laudu?m?a?)|(Instrukcy?j?[ae])|(Instructio)|(Konfederacy?j?)|(Odpowiedź)|(P?okazowan)|(Manifest)|(Protestac)|(Reskrypt)|(Uniwersał)')
+         MONTHS]
+         +
+         # titles - each of those will count as one occurence of a sign
+        [re.compile(s) for s in ['Uchwał[ay]', 'Uniwersał', 'Laudu?m?a?', 'Instrukcy?j?[ae]', 'Instructio', 'Konfederacy?j?', 'Odpowiedź', 'P?okazowan', 'Manifest', 'Protestac', 'Reskrypt', 'Uniwersał', 'Sejmik', 'przedsejmo', 'konkowacyj', 'województwa', 'ziemi']])
+heading_antisigns = ([
+        re.compile('\\D0+'), # isolated zeros are bogus
         ]
+        +
+        # verb endings
+        [re.compile(s) for s in ['y[lł]\\s', '[sś]my\\s', 'y[lł]u\\s', 'aj[ąe]\\s']]
+        +
+        # other out of place vocabulary
+        [re.compile(s, flags=re.IGNORECASE) for s in ['ichm', 'jmp']]
+        )
+
 def is_heading(section, config):
     if len(section) < 15 or len(section) > config['max_heading_len']:
         return False
 
     signs = [s.search(section) for s in heading_signs]
     signs_count = len([s for s in signs if s])
+    antisigns = [s.search(section) for s in heading_antisigns]
+    antisigns_count = len([s for s in antisigns if s])
+    signs_count -= antisigns_count
 ###    print(section, signs_count, 'signs')
 
     return signs_count >= 2 and len(section) > 0 and signs_count > (len(section) / 30)
